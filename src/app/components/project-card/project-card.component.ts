4445e46ee4f97ca30b77816a9bc45516604628d9
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2023-08-07 01:20:10 +0300

    Added information for different projects

--- src/app/components/project-card/project-card.component.ts
+++ src/app/components/project-card/project-card.component.ts
@@ -1,10 +1,18 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, ElementRef, HostListener, Input, Renderer2, ViewChild } from '@angular/core';
 import { Project } from 'src/app/models/project.type';
 
 @Component({
   selector: 'app-project-card',
   templateUrl: './project-card.component.html',
-  styleUrls: ['./project-card.component.scss']
+  styleUrls: ['./project-card.component.scss'],
+  animations: [
+    trigger('expandCollapse', [
+      state('collapsed', style({ height: '0px', minHeight: '0', display: 'none' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('1000ms ease-in-out')),
+    ]),
+  ],
 })
 export class ProjectCardComponent {
   @Input() project: Project | undefined;
@@ -19,11 +27,18 @@ export class ProjectCardComponent {
     window.location.href = this.project!.github;
   }
 
+  isExpanded = false;
+
+  togglePanel() {
+    this.isExpanded = !this.isExpanded;
+  }
+
   open() {
-    (document.querySelector(".content") as HTMLElement).style.visibility = "visible";
+    // (document.querySelector(".content") as HTMLElement).style.visibility = "visible";
   }
 
   close() {
-    (document.querySelector(".content") as HTMLElement).style.visibility = "hidden";
+
+    // (document.querySelector(".content") as HTMLElement).style.visibility = "hidden";
   }
 }
--- src/app/sections/projects/projects.component.ts
+++ src/app/sections/projects/projects.component.ts
@@ -17,28 +17,28 @@ export class ProjectsComponent {
       startToEnd: "06/2023"
     },
     {
-      name: "CharGO",
-      description: "CharGO is a mobile app that aims to revolutionize the way users find and reserve electric car chargers.With a user- friendly interface, CharGO simplifies the process of locating and booking charging stations, providing a seamless experience for electric vehicle owners.I developed the ability to construct and present innovative ideas effectively.",
+      name: "ReportR",
+      description: "ReportR is an innovative app that provides a practical solution to the everyday challenges faced by large organizations, such as schools and businesses.The app aims to simplify the process of managing and resolving issues that can arise in these organizations, making it easier for them to maintain a smooth and efficient operation by user contribution.I gained valuable skills in teamwork, coordination, and adaptability.",
       downloadFileLink: "",
-      github: "https://github.com/mutenoic/CharGO",
-      imageUrl: "https://camo.githubusercontent.com/b9b0ac02657b31fabe92e3240e8eb7278c7ade3879fdd7879eabdd16b345b42d/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f313039303434353336303835393732353836342f313131343439323333393932333230363135342f63686172676f5f6e6f5f62672e706e67",
-      startToEnd: "06/2023"
+      github: "https://github.com/kaloyanstoyanov06/reportr",
+      imageUrl: "https://camo.githubusercontent.com/13a59001149a945b642a91bdc3fee0de4f4335aa09ca986b0173061059cd65c5/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f313034353436333031343837363339373633382f313039353430383838343335343635303232332f63325446676435695f34782e6a7067",
+      startToEnd: "04/2023"
     },
     {
-      name: "CharGO",
-      description: "CharGO is a mobile app that aims to revolutionize the way users find and reserve electric car chargers.With a user- friendly interface, CharGO simplifies the process of locating and booking charging stations, providing a seamless experience for electric vehicle owners.I developed the ability to construct and present innovative ideas effectively.",
+      name: "EducateIO",
+      description: "EducateIO is a cutting-edge mobile application built on the Flutter framework, designed to revolutionize the way individuals find teachers and learn specific subjects. Whether you're a student seeking personalized education or a private tutor who looks to advertise themselves to more students, Another notable feature of the platform is its facilitation of student collaboration and the exchange of knowledge between students. Throughout my experience, I also acquired practical expertise in utilizing Firebase, specifically its authentication and Firestore functionalities, complementing my proficiency in leveraging Android APIs and Google Maps APIs to create feature-rich mobile applications with intuitive interfaces and advanced mapping functionalities.",
       downloadFileLink: "",
-      github: "https://github.com/mutenoic/CharGO",
-      imageUrl: "https://camo.githubusercontent.com/b9b0ac02657b31fabe92e3240e8eb7278c7ade3879fdd7879eabdd16b345b42d/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f313039303434353336303835393732353836342f313131343439323333393932333230363135342f63686172676f5f6e6f5f62672e706e67",
-      startToEnd: "06/2023"
+      github: "https://github.com/KaloyanStoyanov06/educate_io",
+      imageUrl: "https://github.com/KaloyanStoyanov06/educate_io/blob/master/android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png?raw=true",
+      startToEnd: "12/2022 - 02/2023"
     },
     {
-      name: "CharGO",
-      description: "CharGO is a mobile app that aims to revolutionize the way users find and reserve electric car chargers.With a user- friendly interface, CharGO simplifies the process of locating and booking charging stations, providing a seamless experience for electric vehicle owners.I developed the ability to construct and present innovative ideas effectively.",
+      name: "School Program",
+      description: "This mobile application is developed using the Flutter framework and utilizes local storage to store program data.It enables users to conveniently schedule recurring programs from Monday to Friday and set reminders for homework and tests.It's extremely useful for students and teachers. I developed proficiency in utilizing local storage and saving settings within the mobile application, optimizing user experience and data management.",
       downloadFileLink: "",
       github: "https://github.com/mutenoic/CharGO",
-      imageUrl: "https://camo.githubusercontent.com/b9b0ac02657b31fabe92e3240e8eb7278c7ade3879fdd7879eabdd16b345b42d/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f313039303434353336303835393732353836342f313131343439323333393932333230363135342f63686172676f5f6e6f5f62672e706e67",
-      startToEnd: "06/2023"
+      imageUrl: "https://github.com/KaloyanStoyanov06/school_program/blob/main/android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png?raw=true",
+      startToEnd: "09/2022"
     },
   ];
 }
