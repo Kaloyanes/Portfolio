0e6cf439479f6ddc25f14f5bec48d7995f7633b0
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2023-08-07 01:22:43 +0300

    Details button makes you go to a new tab instead of replacing the current one

--- src/app/components/project-card/project-card.component.ts
+++ src/app/components/project-card/project-card.component.ts
@@ -6,39 +6,12 @@ import { Project } from 'src/app/models/project.type';
   selector: 'app-project-card',
   templateUrl: './project-card.component.html',
   styleUrls: ['./project-card.component.scss'],
-  animations: [
-    trigger('expandCollapse', [
-      state('collapsed', style({ height: '0px', minHeight: '0', display: 'none' })),
-      state('expanded', style({ height: '*' })),
-      transition('expanded <=> collapsed', animate('1000ms ease-in-out')),
-    ]),
-  ],
 })
 export class ProjectCardComponent {
   @Input() project: Project | undefined;
 
-
-  print() {
-    console.log("HELLO WORLD");
-
-  }
-
   details() {
-    window.location.href = this.project!.github;
-  }
+    window.open(this.project!.github, "_blank");
 
-  isExpanded = false;
-
-  togglePanel() {
-    this.isExpanded = !this.isExpanded;
-  }
-
-  open() {
-    // (document.querySelector(".content") as HTMLElement).style.visibility = "visible";
-  }
-
-  close() {
-
-    // (document.querySelector(".content") as HTMLElement).style.visibility = "hidden";
   }
 }
