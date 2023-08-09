028d4100fa49116d4a6892a69250c17179c09b83
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2023-08-09 15:30:01 +0300

    added responsive design for skills

--- src/app/components/skill/skill.component.ts
+++ src/app/components/skill/skill.component.ts
@@ -3,11 +3,11 @@ import { Skill } from 'src/app/models/skill.type';
 import { NgIf } from '@angular/common';
 
 @Component({
-    selector: 'app-skill',
-    templateUrl: './skill.component.html',
-    styleUrls: ['./skill.component.scss'],
-    standalone: true,
-    imports: [NgIf],
+  selector: 'app-skill',
+  templateUrl: './skill.component.html',
+  styleUrls: ['./skill.component.scss'],
+  standalone: true,
+  imports: [NgIf],
 })
 export class SkillComponent {
   @Input({ required: true }) skill: Skill | undefined;
