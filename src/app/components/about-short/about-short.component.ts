1526a00879f39c1361cbadc30ec4d92ba7ba0928
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2023-09-04 21:24:30 +0300

    made a background

--- src/app/components/about-short/about-short.component.ts
+++ src/app/components/about-short/about-short.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import gsap from 'gsap';
 
 @Component({
-    selector: 'app-about-short',
-    templateUrl: './about-short.component.html',
-    styleUrls: ['./about-short.component.scss'],
-    standalone: true
+  selector: 'app-about-short',
+  templateUrl: './about-short.component.html',
+  styleUrls: ['./about-short.component.scss'],
+  standalone: true
 })
 export class AboutShortComponent {
+  ngOnInit() {
 
+  }
 }
