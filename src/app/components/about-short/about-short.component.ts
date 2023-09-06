c5ebcf98dd5220ffecfb54829509d953ea2a98a5
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2023-09-06 11:41:14 +0300

    Redesigned about-short

--- src/app/components/about-short/about-short.component.ts
+++ src/app/components/about-short/about-short.component.ts
@@ -1,11 +1,14 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import gsap from 'gsap';
 
 @Component({
   selector: 'app-about-short',
   templateUrl: './about-short.component.html',
   styleUrls: ['./about-short.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [NgOptimizedImage, MatIconModule]
 })
 export class AboutShortComponent {
   ngOnInit() {
