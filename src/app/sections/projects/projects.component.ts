7e07d106cab008689187fbb1968fb275d31645ea
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2023-09-06 11:43:27 +0300

    Added pictures to the projects

--- src/app/sections/projects/projects.component.ts
+++ src/app/sections/projects/projects.component.ts
@@ -4,11 +4,11 @@ import { ProjectCardComponent } from '../../components/project-card/project-card
 import { NgFor } from '@angular/common';
 
 @Component({
-    selector: 'app-projects',
-    templateUrl: './projects.component.html',
-    styleUrls: ['./projects.component.scss'],
-    standalone: true,
-    imports: [NgFor, ProjectCardComponent]
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.scss'],
+  standalone: true,
+  imports: [NgFor, ProjectCardComponent]
 })
 export class ProjectsComponent {
   projects: Project[] = [
@@ -18,7 +18,15 @@ export class ProjectsComponent {
       downloadFileLink: "",
       github: "https://github.com/mutenoic/CharGO",
       imageUrl: "https://camo.githubusercontent.com/b9b0ac02657b31fabe92e3240e8eb7278c7ade3879fdd7879eabdd16b345b42d/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f313039303434353336303835393732353836342f313131343439323333393932333230363135342f63686172676f5f6e6f5f62672e706e67",
-      startToEnd: "06/2023"
+      startToEnd: "06/2023",
+      images: [
+        "assets/projects/CharGO/6.webp",
+        "assets/projects/CharGO/2.webp",
+        "assets/projects/CharGO/1.webp",
+        "assets/projects/CharGO/3.webp",
+        "assets/projects/CharGO/4.webp",
+        "assets/projects/CharGO/5.webp",
+      ],
     },
     {
       name: "ReportR",
@@ -26,7 +34,20 @@ export class ProjectsComponent {
       downloadFileLink: "",
       github: "https://github.com/kaloyanstoyanov06/reportr",
       imageUrl: "https://github.com/KaloyanStoyanov06/reportr/blob/master/android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png?raw=true",
-      startToEnd: "04/2023"
+      startToEnd: "04/2023",
+      images: [
+        "assets/projects/ReportR/1.webp",
+        "assets/projects/ReportR/8.webp",
+        "assets/projects/ReportR/2.webp",
+        "assets/projects/ReportR/3.webp",
+        "assets/projects/ReportR/4.webp",
+        "assets/projects/ReportR/5.webp",
+        "assets/projects/ReportR/6.webp",
+        "assets/projects/ReportR/7.webp",
+        "assets/projects/ReportR/9.webp",
+        "assets/projects/ReportR/10.webp",
+        "assets/projects/ReportR/11.webp",
+      ],
     },
     {
       name: "EducateIO",
@@ -34,7 +55,24 @@ export class ProjectsComponent {
       downloadFileLink: "",
       github: "https://github.com/KaloyanStoyanov06/educate_io",
       imageUrl: "https://github.com/KaloyanStoyanov06/educate_io/blob/master/android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png?raw=true",
-      startToEnd: "12/2022 - 02/2023"
+      startToEnd: "12/2022 - 02/2023",
+      images: [
+        "assets/projects/EducateIO/10.webp",
+        "assets/projects/EducateIO/7.webp",
+        "assets/projects/EducateIO/3.webp",
+        "assets/projects/EducateIO/4.webp",
+        "assets/projects/EducateIO/1.webp",
+        "assets/projects/EducateIO/2.webp",
+        "assets/projects/EducateIO/5.webp",
+        "assets/projects/EducateIO/6.webp",
+        "assets/projects/EducateIO/8.webp",
+        "assets/projects/EducateIO/9.webp",
+        "assets/projects/EducateIO/11.webp",
+        "assets/projects/EducateIO/12.webp",
+        "assets/projects/EducateIO/13.webp",
+        "assets/projects/EducateIO/14.webp",
+      ],
+
     },
     {
       name: "School Program",
@@ -42,7 +80,17 @@ export class ProjectsComponent {
       downloadFileLink: "",
       github: "https://github.com/mutenoic/CharGO",
       imageUrl: "https://github.com/KaloyanStoyanov06/school_program/blob/main/android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png?raw=true",
-      startToEnd: "09/2022"
+      startToEnd: "09/2022",
+      images: [
+        "assets/projects/School/1.webp",
+        "assets/projects/School/2.webp",
+        "assets/projects/School/3.webp",
+        "assets/projects/School/4.webp",
+        "assets/projects/School/5.webp",
+        "assets/projects/School/6.webp",
+        "assets/projects/School/7.webp",
+        "assets/projects/School/8.webp",
+      ],
     },
   ];
 }
