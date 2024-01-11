a54942eb2bd9d2dfb8b3de0f89ba73b3cdb3f5e6
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2024-01-11 19:21:22 +0200

    Made project with angular

--- .firebase/kaloyanes-portfolio/functions/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.85ec320f.js","_app/immutable/chunks/scheduler.b0fd5642.js","_app/immutable/chunks/index.0f1d9fda.js","_app/immutable/chunks/singletons.a2168cba.js"];
-export const stylesheets = [];
-export const fonts = [];
