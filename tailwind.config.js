013a4fc7878edf136ba2ff9b55cefe8177ae136e
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2024-01-13 23:01:08 +0200

    Add daisyui library and update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,23 +4,24 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    colors: {
-      'background': "#0F0F0F",
-      'primary': "#fff",
-      'accent': "#9A7AA0",
-      "card-bg": "#171717",
-    },
     fontFamily: {
       'sans': ['"Roboto"', 'sans-serif'],
       'serif': ['"Roboto"', 'sans-serif'],
     },
     extend: {
-
+      colors: {
+        'background': "#0F0F0F",
+        'primary': "#fff",
+        'accent': "#9A7AA0",
+        "card-bg": "#171717",
+      },
       caretColor: {
         dark: "#fff",
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require("daisyui")
+  ],
 }
 
