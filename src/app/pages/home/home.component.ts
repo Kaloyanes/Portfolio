8a782a8965415154f156327a807dfc9392b6f191
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2024-01-17 10:22:04 +0200

    Added animation on visit that scales up the image and then places it onto place

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,8 +3,10 @@ import { AboutMeComponent } from "./sections/about-me/about-me.component";
 import { ProjectsComponent } from "./sections/projects/projects.component";
 import { ContactMeComponent } from "./sections/contact-me/contact-me.component";
 import { EducationComponent } from "./sections/education/education.component";
-import { EmailService } from '../../services/email.service';
+import { EmailService } from '@services/email.service';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { animate, timeline, stagger } from 'motion';
+import { easeOutBack, easeOutCubic, easeOutElastic } from '@utils/Easings';
 
 @Component({
   selector: 'home',
@@ -14,7 +16,6 @@ import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
   imports: [ReactiveFormsModule, AboutMeComponent, ProjectsComponent, ContactMeComponent, EducationComponent,]
 })
 export class HomeComponent {
-
   email = new FormControl('', {
     nonNullable: true,
     validators: [
@@ -36,6 +37,59 @@ export class HomeComponent {
 
   constructor(public emailService: EmailService) { }
 
+  ngAfterViewInit(): void {
+    timeline(
+      [
+        ['.img-pfp', {
+          scale: [0, 1.3],
+          y: ['-15%', '15%'],
+          opacity: [0, 1],
+        },
+          {
+            duration: 0.5, delay: 0.4, easing: easeOutCubic
+          }
+        ],
+        ['.welcome', {
+          opacity: [0, 1],
+          scale: [0.8, 1],
+          translate: ['-50%', "-50%"],
+          y: ['-50%', '0%'],
+        },
+          {
+            duration: 0.5, delay: 0.4, easing: easeOutCubic, at: "<",
+          }
+        ],
+        [".img-pfp", {
+          scale: [1.3, 1],
+          y: ['15%', 0],
+
+        },
+          { duration: 1, delay: 1.5, easing: easeOutBack, at: "<" }],
+        [".welcome",
+          {
+            opacity: [1, 0],
+            scale: [1, 0.8],
+            translate: ['-50%', "-50%"],
+            y: ['0%', '-50%'],
+          },
+          {
+            at: "<",
+            delay: 1.5,
+          },
+        ],
+        [
+          ".cell:not(.img-pfp)", {
+            opacity: [0, 1],
+            scale: [0.8, 1],
+          }
+          , {
+            duration: 0.8, delay: stagger(0.2, { from: "first" }), easing: easeOutElastic, at: 2
+          }
+        ]
+      ],
+    );
+  }
+
   sendEmail() {
 
     // call sendEmail from contactMeComponent
@@ -45,8 +99,7 @@ export class HomeComponent {
       throw new Error("Email or message is empty");
     }
 
-    (document.querySelector("#contact-me-dialog") as HTMLDialogElement).close();
-
+    this.closeDialog();
 
     this.emailService.sendEmail(
       this.email.value,
@@ -56,8 +109,10 @@ export class HomeComponent {
     this.email.reset();
     this.message.reset();
     this.isSubmitted = false;
+  }
 
-
+  closeDialog() {
+    (document.querySelector('#contact-me-dialog') as HTMLDialogElement).close();
   }
 
 
