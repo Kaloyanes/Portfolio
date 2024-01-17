892a8dad8be2faf93e4e23a1ad43434ee9fcbf7d
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2024-01-17 13:47:46 +0200

    Fix animation and button styling in home component, add projects service

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -38,11 +38,19 @@ export class HomeComponent {
   constructor(public emailService: EmailService) { }
 
   ngAfterViewInit(): void {
+    // if localhost
+    let skipAnimation = true;
+
+    if (document.location.hostname === "localhost" && skipAnimation) {
+      document.querySelector(".welcome")!.classList.add("hidden");
+      return;
+    }
+
     timeline(
       [
         ['.img-pfp', {
-          scale: [0, 1.3],
-          y: ['-15%', '15%'],
+          scale: [0.4, 1.3],
+          y: ['15%', '15%'],
           opacity: [0, 1],
         },
           {
@@ -75,6 +83,7 @@ export class HomeComponent {
           {
             at: "<",
             delay: 1.5,
+            easing: easeOutCubic,
           },
         ],
         [
