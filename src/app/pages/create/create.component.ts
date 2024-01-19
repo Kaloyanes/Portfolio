cde1173b745fb1700d10ce79ad16ce78b55ed820
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2024-01-19 11:55:17 +0200

    Uploading pictures to project creation

--- src/app/pages/create/create.component.ts
+++ src/app/pages/create/create.component.ts
@@ -3,8 +3,8 @@ import { Firestore } from '@angular/fire/firestore';
 import { Auth, signOut } from '@angular/fire/auth';
 import { ProjectsService } from '@services/projects.service';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { Project } from '@models/project';
-import { Storage, uploadBytes } from '@angular/fire/storage';
+import { getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { getDownloadURL, uploadBytes } from '@angular/fire/storage';
 
 @Component({
   selector: 'create',
@@ -14,27 +14,73 @@ import { Storage, uploadBytes } from '@angular/fire/storage';
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
-  constructor(public auth: Auth, public ProjectService: ProjectsService, public Firestore: Firestore, public Storage: Storage,) { }
+  constructor(public auth: Auth, public ProjectService: ProjectsService, public Firestore: Firestore,) { }
 
   name = new FormControl<string>("");
   description = new FormControl<string>("");
   shortDescription = new FormControl<string>("");
   top = new FormControl<boolean>(false);
   position = new FormControl<number>(0);
-  files = new FormControl<File[]>([]);
+  files = new FormControl<FileList | undefined>(undefined);
   images = signal<string[]>([]);
 
   async createProject() {
     let id = this.ProjectService.GenerateSlug(this.name.value!);
     console.log(id, this.name.value!);
 
+    await this.uploadImages(id);
+
+    console.log(this.images());
+
+    this.clearFields();
+  }
+
+  async uploadImages(id: string) {
+    console.log(this.files.value!);
+
+
+    if (this.files.value === null) return;
+
+    var files = this.files.value;
+
+    if (files === undefined) return;
+
+    for (let i = 0; i < files.length; i++) {
+      let file = files.item(i)!;
+
+      let storage = getStorage();
+      console.log(file.name)
+      let reference = ref(storage, `projects/${id}/${file.name}`);
+
+      var sd = await uploadBytes(reference, file);
+      var sf = await getDownloadURL(sd.ref);
+
+      this.images.set([...this.images(), sf])
+      console.log(sf);
+    }
+  }
+
+  onImagePicked($event: Event) {
+    let target = $event.target as HTMLInputElement;
+
+    if (target.files === null) return;
+
+    var files = target.files;
+
+    this.files.setValue(files);
+  }
+
+  clearFields() {
+    this.name.setValue("");
+    this.description.setValue("");
+    this.shortDescription.setValue("");
+    this.top.setValue(false);
+    this.position.setValue(0);
+    this.files.setValue(undefined);
   }
 
   async signOut() {
     await signOut(this.auth);
     window.location.href = "/home";
   }
-
-
-
 }
