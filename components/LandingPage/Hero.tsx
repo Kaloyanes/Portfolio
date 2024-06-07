d39d52017ea2bdd06c82d45031a798dc61b70576
Kaloyanes <kaloyangfx@gmail.com>
2024-06-07 08:57:18 +0300

    Add text animation

--- components/LandingPage/Hero.tsx
+++ components/LandingPage/Hero.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { motion, useAnimate } from "framer-motion";
+import { animate, motion, stagger } from "framer-motion";
 import { Noto_Sans } from "next/font/google";
 import { useEffect } from "react";
 import SplitType from "split-type";
@@ -9,21 +9,43 @@ const roboto = Noto_Sans({
   weight: ["400", "700", "900"],
 });
 export default function Hero() {
-  const [scope, animate] = useAnimate();
-
   useEffect(() => {
     const text = new SplitType("#firstName");
+    const lastNameText = new SplitType("#lastName");
+
+    const transitionProperties = {
+      duration: 0.3,
+      delay: stagger(0.07),
+      type: "spring",
+      stiffness: 125,
+    };
+
+    const animationProperties = {
+      y: [50, 0],
+      opacity: [0, 1],
+    };
+    animate(
+      text.chars!,
+      animationProperties as any,
+      transitionProperties as any,
+    );
+
+    animate(
+      lastNameText.chars!,
+      animationProperties as any,
+      transitionProperties as any,
+    );
   });
 
   return (
-    <div className={roboto.className} ref={scope}>
+    <div className={roboto.className}>
       <motion.h1
         id="firstName"
         className="text-left text-9xl absolute font-bold "
       >
         Kaloyan
       </motion.h1>
-      <motion.h1 className="text-right text-9xl font-bold">
+      <motion.h1 id="lastName" className="text-right text-9xl font-bold">
         Stoyan
         <br />o<br />v
       </motion.h1>
